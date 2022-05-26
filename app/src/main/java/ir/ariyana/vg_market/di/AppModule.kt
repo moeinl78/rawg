8f50f2e9992d47d85312a3d649462e8e7f98eb9b
Moein Latifi <moeinl1090@protonmail.com>
2022-05-26 12:12:43 +0430

    create dependency for repository

--- app/src/main/java/ir/ariyana/vg_market/di/AppModule.kt
+++ app/src/main/java/ir/ariyana/vg_market/di/AppModule.kt
@@ -1,7 +1,24 @@
 package ir.ariyana.vg_market.di
 
+import ir.ariyana.vg_market.repository.MainRepository
+import ir.ariyana.vg_market.repository.remote.ServiceAPI
+import ir.ariyana.vg_market.utils.Constants
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val appModule = module {
 
+    single {
+        Retrofit
+            .Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ServiceAPI::class.java)
+    }
+
+    factory {
+        MainRepository(get())
+    }
 }
