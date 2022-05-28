7eefac5c064ad6646f8ea10aa0f7ec15dc574beb
Moein Latifi <moeinl1090@protonmail.com>
2022-05-28 20:19:43 +0430

    sent data to adapter in home fragment

--- app/src/main/java/ir/ariyana/vg_market/ui/fragment/HomeFragment.kt
+++ app/src/main/java/ir/ariyana/vg_market/ui/fragment/HomeFragment.kt
@@ -5,11 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import ir.ariyana.vg_market.databinding.FragmentHomeBinding
+import ir.ariyana.vg_market.model.VideoGameList
+import ir.ariyana.vg_market.ui.adapter.AdapterRecycler
+import ir.ariyana.vg_market.ui.main.MainViewModel
+import ir.ariyana.vg_market.utils.Resource
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class HomeFragment: Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var adapter: AdapterRecycler
+    private val mainViewModel by sharedViewModel<MainViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,6 +30,29 @@ class HomeFragment: Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+
+        mainViewModel.getGameList()
+
+        mainViewModel
+            .gameList
+            .observe(viewLifecycleOwner) { res ->
+
+                when (res) {
+
+                    is Resource.Success -> {
+                        adapter = AdapterRecycler(res.data?.results as ArrayList<VideoGameList.Result>)
+                        binding.fragmentHomeRecyclerView.adapter = adapter
+                        binding.fragmentHomeRecyclerView.layoutManager = LinearLayoutManager(binding.root.context, RecyclerView.HORIZONTAL, false)
+                    }
+
+                    is Resource.Error -> {
+
+                    }
+
+                    is Resource.Loading -> {
+
+                    }
+                }
+            }
     }
 }
